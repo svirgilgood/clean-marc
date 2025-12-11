cc6a04cf581422dbefcf45a4f6a1b5257260c315
svirgilgood <svirgilgoodwin@gmail.com>
2025-12-11 09:42:24 -0700

    updating the queries to be in the areas they need to be

--- src/clean_marc/queries/queries.py
+++ src/clean_marc/queries/queries.py
@@ -1,16 +1,25 @@
 from pathlib import Path
 from typing import List, Dict
 
-query_dir = Path(__file__).parent
+from ..utils import QuerDir
+
+base_dir = Path(__file__).parent
 
 
-def collect_queries() -> List[Dict[str, str]]:
+def collect_queries(dir=QuerDir.ITEM) -> List[Dict[str, str]]:
     """
     Read the contents of the queries directory to create
     queries
     """
+
+    match dir:
+        case QuerDir.ITEM:
+            query_dir = base_dir / "item_queries"
+        case QuerDir.RECORD:
+            query_dir = base_dir / "record_queries"
+
     queries: List[Dict[str, str]] = []
-    for query in query_dir.glob("*.rq"):
+    for query in (base_dir / dir.value).glob("*.rq"):
         basename = query.stem
         with open(query, "r") as fp:
             queries.append({"query_name": basename, "query": fp.read()})
